aec1aa284124374679efa611e405b224cf9aac3e
resghlin <141161951+resghlin@users.noreply.github.com>
2026-04-05 16:49:24 -0600

    Add files via upload

---
+++ header0.php
@@ -0,0 +1,47 @@
+<?php
+
+function getRealPath(){
+	/****************************************************************	
+	// remove the sub path information from $path, 
+	$uri =  /~ghlin/log.php	[REQUEST_URI]
+	//$path = /usr/brule2/cshome/ghlin/web_docs/log.php [PATH_TRANSLATED ]
+	$path = /compsci/brule2/cshome/ghlin/web_docs/log.php [PATH_TRANSLATED ]
+	want to get $path = /compsci/brule2/cshome/ghlin/web_docs
+	****************************************************************	*/
+
+	$path = dirname($_SERVER['PATH_TRANSLATED']);		// get the path on disk
+	$uri  = dirname($_SERVER['REQUEST_URI']);			// get Url part after domain name : /~ghlin/***
+	$path = substr($path, 0, strlen($path) - strlen($uri)+7); // where 7 is  length of /~ghlin
+	return $path;
+}
+
+?>
+<!-- start of header logo table -->	
+<style type="text/css">
+<!--
+.style1 {color: #FFFFFF; font-size:11px;}
+-->
+</style>
+
+<TABLE align=center cellSpacing=5 cellPadding=0 width="980" border=0 bgColor=darkgreen>
+   	<TR>
+    <TD width="63%" valign="middle">
+	<!--A href="http://www.digits.com/">
+	<IMG src="http://counter.digits.com/wc/-d/5/-r/-f/ffffff/-b/006633/-c/0/ghlin" align=left height=15 hspace=4 vspace=2></A>
+	<font size=2 color="#fffff"><I>th</I> access</font-->
+	<!-- since 08/28/2001 15:06:11 MDT-->
+    <TD width="37%" align="center" valign="middle">
+	<DIV class="nounderline"><span class="style1">
+	<a href="http://www.cs.ualberta.ca/">Department of</a><br>
+	<a href="http://www.cs.ualberta.ca/"><span style="font-size: 24px">Computing Science</span></a><br></span><span class="style1">
+	<a href="http://www.ualberta.ca/">University of Alberta</a></span></DIV>
+    </TD>
+  	</TR>
+</TABLE>
+<TABLE align=center cellSpacing=5 cellPadding=0 width="980" border=0 bgColor=gold>
+	<TR>
+	<TD><DIV class="nounderline"><strong><a href="http://www.cs.ualberta.ca/~ghlin/"><font color=darkgreen>Guohui Lin's Homepage</font></a></strong>
+	| <strong><a href="http://www.cs.ualberta.ca/~ghlin/mycalendar.php"><font color=darkgreen>Book time</font></a></strong></DIV></TD>
+	</TR>
+</TABLE>
+<!-- End of header logo table -->	
